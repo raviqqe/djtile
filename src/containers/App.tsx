08882aa1ca5cd007c6bbea7cf896d89bb2283fe9
Yota Toyama <raviqqe@gmail.com>
2018-05-31 19:43:14 +0900

    Move App.tsx

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import Tiles from "./containers/Tiles";
+import Tiles from "./Tiles";
 
 class App extends React.Component {
     public render() {
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { injectGlobal } from "styled-components";
 
-import App from "./App";
+import App from "./containers/App";
 
 injectGlobal`
     body {
